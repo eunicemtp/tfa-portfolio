376942c78a9b5bebcdd6bf9795337fc77afae313
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2024-03-15 22:48:37 +0100

    add Trianglify / setting up home page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.info('Hello world');
+document.addEventListener("DOMContentLoaded", function() {
+    var bg = new Trianglify({
+      noiseIntensity: 0,
+      cell_size: 50,
+      cellpadding: 10,
+      fillOpacity: 1,
+      strokeOpacity: 0,
+      x_gradient: ['#311547', '#220334']
+    });
+  
+    var pattern = bg.generate(window.innerWidth, window.innerHeight);
+    document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+  });
+  
